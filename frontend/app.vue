82fd7cf1322b29bea7f7532fffe48bb4f3bb9790
Gilbert Tsurwa <tsurwagilbert@gmail.com>
2023-01-09 11:35:01 -0500

    Storyblok to Sanity.io & Nuxt 3 Upgrade - i successfully replaced Storyblok with Sanity - i also successfully incorporated Nuxt 3 - tested by making a query and things seem to be working fine - note: sanity.config.js replaces sanity.json in Sanity v2

---
+++ frontend/app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <NuxtWelcome />
+  </div>
+</template>
+
+<script setup>
+const query = groq`*[_type == "post"]`;
+const { data } = await useSanityQuery(query);
+console.log(data);
+</script>
