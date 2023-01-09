ce54e88218dc5e1aeae3f6a6077f2b68cd16e762
Gilbert Tsurwa <tsurwagilbert@gmail.com>
2023-01-09 14:41:53 -0500

    Duplicated Post Schema for Personal & Tech Posts

--- api/schemas/index.js
+++ api/schemas/index.js
@@ -1,6 +1,7 @@
 import blockContent from './blockContent'
 import category from './category'
-import post from './post'
+import personal from './posts/personal'
+import tech from './posts/tech'
 import author from './author'
 
-export const schemaTypes = [post, author, category, blockContent]
+export const schemaTypes = [personal, tech, author, category, blockContent]
