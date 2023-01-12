3daf92371f15c3770095584ca4d5862e8c58f091
Gilbert Tsurwa <tsurwagilbert@gmail.com>
2023-01-12 14:40:22 -0500

    Personal & Tech Intro Pages - created a schema (about.js) for the newly created personal and tech intro pages - also used the <SanityContent/> component to properly display my blockContent

--- api/schemas/index.js
+++ api/schemas/index.js
@@ -4,5 +4,6 @@ import author from './author'
 import personal from './posts/personal'
 import tech from './posts/tech'
 import homeGrid from './homeGrid'
+import about from './about'
 
-export const schemaTypes = [homeGrid, personal, tech, author, category, blockContent]
+export const schemaTypes = [about, homeGrid, personal, tech, author, category, blockContent]
